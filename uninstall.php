9fde0097c9b04e831ba950d239fa6b25cd6cdb6a
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-11 13:34:56 -0800

    Update the uninstaller for the new settings configs.

--- uninstall.php
+++ uninstall.php
@@ -15,25 +15,24 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 // Load Custom Login
 include_once 'custom-login.php';
-include_once __DIR__ . '/includes/default-settings.php';
-include_once __DIR__ . '/includes/class-cl-common.php';
+$settings = include_once __DIR__ . '/config/settings.php';
 
 // Delete all plugin options
-if (!empty($sections)) {
-    foreach ($sections as $section) {
+if (!empty($settings['sections'])) {
+    foreach ($settings['sections'] as $section) {
         delete_option($section['id']);
     }
 }
 
 // Delete user meta data
-$all_user_ids = get_users('fields=ID');
+$all_user_ids = get_users(['fields' => 'ID']);
 foreach ($all_user_ids as $user_id) {
-    delete_user_meta($user_id, CUSTOM_LOGIN_OPTION . '_ignore_announcement');
+    delete_user_meta($user_id, 'custom_login_ignore_announcement');
 }
 
 // Delete all announcement options and transients
 delete_transient(CL_Common::get_transient_key('announcement'));
-delete_option(CUSTOM_LOGIN_OPTION . '_announcement_message');
+delete_option('custom_login_announcement_message');
 
 // Delete tracking options
 delete_option('custom_login_tracking_last_send');
@@ -44,4 +43,4 @@ wp_clear_scheduled_hook('custom_login_daily_scheduled_events');
 wp_clear_scheduled_hook('custom_login_weekly_scheduled_events');
 
 // Delete version option
-delete_option(CUSTOM_LOGIN_OPTION . '_version');
+delete_option('custom_login_version');
