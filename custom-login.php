3e3fc7af3112c9d89811db1965ab230221d2cbc5
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-18 12:06:07 -0700

    Add the new Login class.

--- custom-login.php
+++ custom-login.php
@@ -15,19 +15,19 @@
  * @copyright 2012 - 2022
  * @author Austin Passy
  * @link https://austin.passy.co/
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-
 use Dwnload\WpSettingsApi\WpSettingsApi;
 use TheFrosty\CustomLogin\ServiceProvider;
 use TheFrosty\CustomLogin\Settings\Api\Factory;
 use TheFrosty\CustomLogin\Settings\Settings;
 use TheFrosty\CustomLogin\WpAdmin\Dashboard;
+use TheFrosty\CustomLogin\WpLogin\Login;
 use TheFrosty\WpUtilities\Plugin\PluginFactory;
 
 if (is_readable(__DIR__ . '/vendor/autoload.php')) {
@@ -35,10 +35,12 @@ if (is_readable(__DIR__ . '/vendor/autoload.php')) {
 }
 
 $plugin = PluginFactory::create('custom-login', __FILE__);
+/** Container object. @var TheFrosty\WpUtilities\Plugin\Container $container */
 $container = $plugin->getContainer();
 $container->register(new ServiceProvider());
 $plugin
     ->addOnHook(Dashboard::class, 'load-index.php', 5, true, [Dashboard::getArgs()])
+    ->addOnHook(Login::class, 'init', 2, null, [$container])
     ->addOnHook(Settings::class, 'init', 10, true, [$container])
     ->addOnHook(WpSettingsApi::class, 'init', 10, true, [Factory::getPluginSettings($plugin)]);
 
