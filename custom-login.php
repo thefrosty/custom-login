c3f0ab998347eaf1a58bb64c263708ac5b22968f
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-23 11:20:10 -0700

    Update version to 4.0.0, and add functional messaging notices for possible legacy PHP versions or missing composer autoload.

--- custom-login.php
+++ custom-login.php
@@ -3,7 +3,7 @@
  * Plugin Name: Custom Login
  * Plugin URI: https://frosty.media/plugins/custom-login
  * Description: A simple way to customize your WordPress <code>wp-login.php</code> screen! A <a href="https://frosty.media/">Frosty Media</a> plugin.
- * Version: 4.0
+ * Version: 4.0.0
  * Author: Austin Passy
  * Author URI: https://austin.passy.co
  * Requires PHP: 7.4
@@ -30,10 +30,39 @@ use TheFrosty\CustomLogin\WpAdmin\Dashboard;
 use TheFrosty\CustomLogin\WpLogin\Login;
 use TheFrosty\WpUtilities\Plugin\PluginFactory;
 
-if (is_readable(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
+/**
+ * Maybe trigger an error notice "message" on the `shutdown` action hook.
+ * Uses an anonymous function which required PHP >= 5.3.
+ */
+add_action('shutdown', function () {
+    $message = apply_filters('custom_login_shutdown_error_message', '');
+    if (empty($message)) {
+        return;
+    }
+    load_plugin_textdomain('custom-login', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+    echo wp_kses_post(sprintf('<div class="error">%s</div>', wpautop($message)));
+});
+
+if (version_compare(PHP_VERSION, '7.4', '<')) {
+    return add_filter('custom_login_shutdown_error_message', function () {
+        return sprintf(
+            esc_html__(
+                'Custom Login 4.x requires PHP version >= 7.4, you are running %s, plugin is currently NOT ACTIVE.',
+                'custom-login'
+            ),
+            PHP_VERSION
+        );
+    });
+} elseif (!is_readable(__DIR__ . '/vendor/autoload.php')) {
+    return add_filter('custom_login_shutdown_error_message', function () {
+        return esc_html__(
+            'Custom Login can\'t find the autoload file, plugin is currently NOT ACTIVE.',
+            'custom-login'
+        );
+    });
 }
 
+require_once __DIR__ . '/vendor/autoload.php';
 $plugin = PluginFactory::create('custom-login', __FILE__);
 /** Container object. @var TheFrosty\WpUtilities\Plugin\Container $container */
 $container = $plugin->getContainer();
@@ -48,12 +77,12 @@ add_action('plugins_loaded', static function () use ($plugin): void {
     $plugin->initialize();
 });
 
-/**
- * The main function responsible for returning the one true Instance to function everywhere.
- * Use this function like you would a global variable, except without needing to declare the global.
- * @deprecated 4.0.0
- */
 if (!function_exists('CUSTOMLOGIN')) {
+    /**
+     * The main function responsible for returning the one true Instance to function everywhere.
+     * Use this function like you would a global variable, except without needing to declare the global.
+     * @deprecated 4.0.0
+     */
     function CUSTOMLOGIN()
     {
         _deprecated_function(__FUNCTION__, '4.0.0');
