e532f7d51cf233aa12b38710324e9ca08ead6122
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-25 12:22:11 -0700

    Add the new Extensions options page with configs and styles.

---
+++ config/extensions.php
@@ -0,0 +1,112 @@
+<?php declare(strict_types=1);
+
+return [
+    [
+        'title' => 'Custom Login Stealth Login',
+        'description' => 'Protect your wp-login.php page from brute force attacks.',
+        'url' => 'https://frosty.media/plugins/custom-login-stealth-login/',
+        'image' => 'https://i.imgur.com/mhuymPG.jpg',
+        'download_id' => '108',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '108',
+                'price_id' => '1',
+                'price' => '$35',
+            ],
+            [
+                'description' => 'Plus',
+                'download_id' => '108',
+                'price_id' => '2',
+                'price' => '$95',
+            ],
+            [
+                'description' => 'Professional',
+                'download_id' => '108',
+                'price_id' => '3',
+                'price' => '$195',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Custom Login Page Template',
+        'description' => 'Add a login form to any WordPress page.',
+        'url' => 'https://frosty.media/plugins/custom-login-page-template/',
+        'image' => 'https://i.imgur.com/A0rzS9q.jpg',
+        'download_id' => '120',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '120',
+                'price_id' => '1',
+                'price' => '$35',
+            ],
+            [
+                'description' => 'Plus',
+                'download_id' => '120',
+                'price_id' => '2',
+                'price' => '$95',
+            ],
+            [
+                'description' => 'Professional',
+                'download_id' => '120',
+                'price_id' => '3',
+                'price' => '$195',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Custom Login Redirects',
+        'description' => 'Manage redirects after logging in.',
+        'url' => 'https://extendd.com/plugin/wordpress-login-redirects/',
+        'image' => 'https://i.imgur.com/aNGoyAa.jpg',
+        'download_id' => '124',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '124',
+                'price_id' => '1',
+                'price' => '$35',
+            ],
+            [
+                'description' => 'Plus',
+                'download_id' => '124',
+                'price_id' => '2',
+                'price' => '$95',
+            ],
+            [
+                'description' => 'Professional',
+                'download_id' => '124',
+                'price_id' => '3',
+                'price' => '$195',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Custom Login No Password',
+        'description' => 'Allow users to login without a password.',
+        'url' => 'https://frosty.media/plugins/custom-login-no-passowrd-login/',
+        'image' => 'https://i.imgur.com/7SXIpi5.jpg',
+        'download_id' => '128',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '128',
+                'price_id' => '1',
+                'price' => '$35',
+            ],
+            [
+                'description' => 'Plus',
+                'download_id' => '128',
+                'price_id' => '2',
+                'price' => '$95',
+            ],
+            [
+                'description' => 'Professional',
+                'download_id' => '128',
+                'price_id' => '3',
+                'price' => '$195',
+            ],
+        ],
+    ],
+];
