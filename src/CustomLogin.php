ef14940b1ecf0a41aa3fd12ca57de689e8ccc8fd
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-08-09 12:04:34 -0700

    Tag version 4.0.7.

--- src/CustomLogin.php
+++ src/CustomLogin.php
@@ -25,7 +25,7 @@ class CustomLogin extends AbstractHookProvider
 
     public const API_URL = 'https://frosty.media/';
     public const OPTION = 'custom_login';
-    public const VERSION = '4.0.6';
+    public const VERSION = '4.0.7';
 
     /**
      * Get the API URL.
