030b84b7b43d7617eef4ae6e2f9d2992481b498e
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-08-09 11:30:21 -0700

    Add first initial class test.

---
+++ tests/unit/CustomLoginTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace TheFrosty\Tests\CustomLogin;
+
+use TheFrosty\CustomLogin\CustomLogin;
+
+/**
+ * Class CustomLoginTest
+ * @package TheFrosty\Tests\CustomLogin
+ */
+class CustomLoginTest extends TestCase
+{
+
+    private CustomLogin $custom_login;
+
+    /**
+     * Setup.
+     */
+    public function setUp(): void
+    {
+        $this->custom_login = new CustomLogin();
+        $this->reflection = $this->getReflection($this->custom_login);
+    }
+
+    /**
+     * Teardown.
+     */
+    public function tearDown(): void
+    {
+        unset($this->custom_login);
+    }
+
+    /**
+     * Test CustomLogin.
+     */
+    public function testCustomLogin(): void
+    {
+        $this->assertInstanceOf(CustomLogin::class, $this->custom_login);
+        $this->assertCount(3, $this->reflection->getConstants());
+    }
+}
