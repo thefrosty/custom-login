c089b42ec7587a4961f8b220ca44ffe471a542d6
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-11-15 15:08:04 -0800

    Update extensions page.

--- config/extensions.php
+++ config/extensions.php
@@ -3,6 +3,7 @@
 return [
     [
         'title' => 'Custom Login Stealth Login',
+        'slug' => 'custom-login-stealth-login/custom-login-stealth-login.php',
         'description' => 'Protect your wp-login.php page from brute force attacks.',
         'url' => 'https://frosty.media/plugins/custom-login-stealth-login/',
         'image' => 'https://i.imgur.com/mhuymPG.jpg',
@@ -30,6 +31,7 @@ return [
     ],
     [
         'title' => 'Custom Login Page Template',
+        'slug' => 'custom-login-page-template/custom-login-page-template.php',
         'description' => 'Add a login form to any WordPress page.',
         'url' => 'https://frosty.media/plugins/custom-login-page-template/',
         'image' => 'https://i.imgur.com/A0rzS9q.jpg',
@@ -57,6 +59,7 @@ return [
     ],
     [
         'title' => 'Custom Login Redirects',
+        'slug' => 'custom-login-redirects/custom-login-redirects.php',
         'description' => 'Manage redirects after logging in.',
         'url' => 'https://extendd.com/plugin/wordpress-login-redirects/',
         'image' => 'https://i.imgur.com/aNGoyAa.jpg',
@@ -84,6 +87,7 @@ return [
     ],
     [
         'title' => 'Custom Login No Password',
+        'slug' => 'custom-login-no-password/custom-login-no-password.php',
         'description' => 'Allow users to login without a password.',
         'url' => 'https://frosty.media/plugins/custom-login-no-passowrd-login/',
         'image' => 'https://i.imgur.com/7SXIpi5.jpg',
@@ -106,6 +110,54 @@ return [
                 'download_id' => '128',
                 'price_id' => '3',
                 'price' => '$195',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Custom Login Style Pack #1',
+        'slug' => 'custom-login-style-pack-1/custom-login-style-pack-1.php',
+        'description' => 'Quickly create custom login styles on the fly!',
+        'url' => 'https://frosty.media/plugins/custom-login-style-pack-1/',
+        'image' => 'https://i.imgur.com/mhuymPG.jpg',
+        'download_id' => '24815',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '24815',
+                'price_id' => '1',
+                'price' => '$10',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Custom Login Style Pack #2',
+        'slug' => 'custom-login-style-pack-2/custom-login-style-pack-2.php',
+        'description' => 'Quickly create custom login styles on the fly!',
+        'url' => 'https://frosty.media/plugins/custom-login-style-pack-2/',
+        'image' => 'https://i.imgur.com/mhuymPG.jpg',
+        'download_id' => '37523',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '37523',
+                'price_id' => '1',
+                'price' => '$10',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Custom Login Style Pack #3',
+        'slug' => 'custom-login-style-pack-3/custom-login-style-pack-3.php',
+        'description' => 'Quickly create custom login styles on the fly!',
+        'url' => 'https://frosty.media/plugins/custom-login-style-pack-3/',
+        'image' => 'https://i.imgur.com/mhuymPG.jpg',
+        'download_id' => '224197',
+        'links' => [
+            [
+                'description' => 'Personal',
+                'download_id' => '224197',
+                'price_id' => '1',
+                'price' => '$10',
             ],
         ],
     ],
--- resources/views/settings/extensions.php
+++ resources/views/settings/extensions.php
@@ -1,17 +1,23 @@
-<?php declare(strict_types=1);
+<?php
 
+declare(strict_types=1);
+
+$all_plugins = function_exists('get_plugins') ? get_plugins() : [];
 $checkout_url ??= TheFrosty\CustomLogin\CustomLogin::getApiUrl();
 $extensions ??= [];
 ?>
 <div class="wrap">
-    <h2><?php esc_html_e('Available Custom Login Extensions', 'custom-login'); ?></h2>
+    <h2><?php
+        esc_html_e('Available Custom Login Extensions', 'custom-login'); ?></h2>
     <form method="post">
         <div class="section">
-            <?php foreach ($extensions as $key => $extension) { ?>
+            <?php
+            foreach ($extensions as $key => $extension) { ?>
                 <div class="col addon">
                     <div class="addon-container">
                         <div class="img-wrap">
-                            <a href="<?php echo esc_url(
+                            <a href="<?php
+                            echo esc_url(
                                 add_query_arg([
                                     'utm_source' => 'plugin-extensions-page',
                                     'utm_medium' => 'custom-login',
@@ -19,56 +25,81 @@ $extensions ??= [];
                                     'utm_content' => esc_attr($extension['title']),
                                 ], $extension['url'])
                             ); ?>" target="_blank">
-                                <img class="thumbnail" src="<?php echo esc_url($extension['image']); ?>" alt="">
+                                <img class="thumbnail" src="<?php
+                                echo esc_url($extension['image']); ?>" alt="">
                             </a>
-                            <p><?php echo wp_kses_post($extension['description']); ?></p>
+                            <p><?php
+                                echo wp_kses_post($extension['description']); ?></p>
                         </div>
 
-                        <h3><?php echo esc_html($extension['title']); ?></h3>
+                        <h3><?php
+                            echo esc_html($extension['title']); ?></h3>
                         <div class="status" data-status="not-installed" style="display:none">
-                            <p><?php esc_html_e('Not Installed', 'custom-login'); ?></p>
+                            <?php
+                            if (!array_key_exists($extension['slug'], $all_plugins)) {
+                                printf('<p>%s</p>', esc_html__('Not Installed', 'custom-login'));
+                            } elseif (is_plugin_inactive($extension['slug'])) {
+                                printf('<p>%s</p>', esc_html__('Installed - Not Active', 'custom-login'));
+                            } else {
+                                printf('<p>%s</p>', esc_html__('Active', 'custom-login'));
+                            }
+                            ?>
                         </div>
                         <hr>
                         <a class="button primary show-if-not-purchased"
-                           href="<?php echo esc_url(
+                           href="<?php
+                           echo esc_url(
                                add_query_arg(
                                    [
                                        'edd_action' => 'add_to_cart',
                                        'download_id' => $extension['download_id'],
                                    ],
                                    TheFrosty\CustomLogin\CustomLogin::getApiUrl('checkout/')
-                               )); ?>" target="_blank"
-                           data-toggle="purchase-links-<?php echo esc_attr($key); ?>"
-                           style="display:none"><?php esc_html_e('Get this Extension', 'custom-login'); ?></a>
-                        <div id="purchase-links-<?php echo esc_attr($key); ?>" style="display:none">
+                               )
+                           ); ?>" target="_blank"
+                           data-toggle="purchase-links-<?php
+                           echo esc_attr($key); ?>"
+                           style="display:none"><?php
+                            esc_html_e('Get this Extension', 'custom-login'); ?></a>
+                        <div id="purchase-links-<?php
+                        echo esc_attr($key); ?>" style="display:none">
                             <ul>
-                                <?php foreach ($extension['links'] as $link) { ?>
+                                <?php
+                                foreach ($extension['links'] as $link) { ?>
                                     <li>
-                                        <?php echo wp_kses_post($link['description']); ?>
-                                        (<?php echo esc_html($link['price']); ?>):
-                                        <a href="<?php echo esc_url(
+                                        <?php
+                                        echo wp_kses_post($link['description']); ?>
+                                        (<?php
+                                        echo esc_html($link['price']); ?>):
+                                        <a href="<?php
+                                        echo esc_url(
                                             add_query_arg([
                                                 'edd_action' => 'straight_to_gateway',
                                                 'download_id' => $link['download_id'],
                                                 'edd_options[price_id]' => $link['price_id'],
                                             ], $checkout_url)
-                                        ); ?>"><?php esc_html_e('PayPal', 'custom-login'); ?></a>
+                                        ); ?>"><?php
+                                            esc_html_e('PayPal', 'custom-login'); ?></a>
                                         |
-                                        <a href="<?php echo esc_url(
+                                        <a href="<?php
+                                        echo esc_url(
                                             add_query_arg([
                                                 'edd_action' => 'add_to_cart',
                                                 'download_id' => $link['download_id'],
                                                 'edd_options[price_id]' => $link['price_id'],
                                             ], $checkout_url)
-                                        ); ?>"><?php esc_html_e('Credit Card', 'custom-login'); ?></a>
+                                        ); ?>"><?php
+                                            esc_html_e('Credit Card', 'custom-login'); ?></a>
                                     </li>
-                                <?php } // Links ?>
+                                    <?php
+                                } // Links ?>
                             </ul>
 
                         </div>
                     </div>
                 </div>
-            <?php } // Extensions ?>
+                <?php
+            } // Extensions ?>
         </div>
     </form>
     <script type="text/javascript">
