44e5471ff82d80844d9937852e6da8fb9b77859a
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-04-14 11:33:40 -0700

    Translation loading for the custom-login domain was triggered too early.

--- src/CustomLogin.php
+++ src/CustomLogin.php
@@ -69,7 +69,7 @@ class CustomLogin extends AbstractHookProvider
     {
         $this->addFilter('plugin_action_links', [$this, 'pluginActionLinks'], 10, 2);
         $this->addFilter('plugin_row_meta', [$this, 'pluginRowMeta'], 10, 2);
-        $this->addAction('plugins_loaded', [$this, 'i18n']);
+        $this->addAction('init', [$this, 'i18n']);
         $this->addAction('login_head', [$this, 'metaGenerator'], 1);
         do_action(Factory::PREFIX . 'actions');
     }
