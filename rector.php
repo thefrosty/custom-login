46b173b0b0152b3a5cbcd51f1111929ab9748a46
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-06-12 12:17:40 -0700

    Add additional directories

--- rector.php
+++ rector.php
@@ -9,5 +9,7 @@ return RectorConfig::configure()
     ->withPhpVersion(PhpVersion::PHP_83)
     ->withPaths([
         __DIR__ . '/src',
+        __DIR__ . '/vendor/dwnload/',
+        __DIR__ . '/vendor/thefrosty/wp-utilities',
     ])
     ->withDowngradeSets(php74: true);
