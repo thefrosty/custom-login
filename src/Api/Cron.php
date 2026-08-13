d7303c6bc29bf3f6147de58d33ec911558f0c7db
Austin Passy <367897+thefrosty@users.noreply.github.com>
2026-08-13 09:31:08 -0700

    Class constant type is missing

--- src/Api/Cron.php
+++ src/Api/Cron.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace TheFrosty\CustomLogin\Api;
 
@@ -15,8 +17,8 @@ use function wp_schedule_event;
 class Cron extends AbstractHookProvider
 {
 
-    public const HOOK_DAILY = 'custom_login_daily_scheduled_events';
-    public const HOOK_WEEKLY = 'custom_login_weekly_scheduled_events';
+    public const string HOOK_DAILY = 'custom_login_daily_scheduled_events';
+    public const string HOOK_WEEKLY = 'custom_login_weekly_scheduled_events';
 
     /**
      * Add class hooks.
